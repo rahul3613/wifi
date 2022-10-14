55ad15df18a920da101487d605727510ae4eb3e3
Rahul Vishwakarma <53302959+rahul3613@users.noreply.github.com>
2022-10-14 15:24:02 +0530

    no rssi

--- wifi.py
+++ wifi.py
@@ -1,10 +1,48 @@
-import rssi
+from scapy.all import *
+from threading import Thread
+import pandas
+import time
+import os
 
-interface = 'wlan0'
-rssi_scanner = rssi.RSSI_Scan(interface)
 
-ssids = ['NISER-AP']
+# initialize the networks dataframe that will contain all access points nearby
+networks = pandas.DataFrame(columns=["BSSID", "SSID", "dBm_Signal", "Channel", "Crypto"])
+# set the index BSSID (MAC address of the AP)
+networks.set_index("BSSID", inplace=True)
 
-ap_info = rssi_scanner.getAPinfo()
 
-print(ap_info)
+def callback(packet):
+    if packet.haslayer(Dot11Beacon):
+        # extract the MAC address of the network
+        bssid = packet[Dot11].addr2
+        # get the name of it
+        ssid = packet[Dot11Elt].info.decode()
+        try:
+            dbm_signal = packet.dBm_AntSignal
+        except:
+            dbm_signal = "N/A"
+        # extract network stats
+        stats = packet[Dot11Beacon].network_stats()
+        # get the channel of the AP
+        channel = stats.get("channel")
+        # get the crypto
+        crypto = stats.get("crypto")
+        networks.loc[bssid] = (ssid, dbm_signal, channel, crypto)
+
+
+def print_all():
+    while True:
+        os.system("clear")
+        print(networks)
+        time.sleep(0.5)
+
+
+if __name__ == "__main__":
+    # interface name, check using iwconfig
+    interface = "wifi0"
+    # start the thread that prints all the networks
+    printer = Thread(target=print_all)
+    printer.daemon = True
+    printer.start()
+    # start sniffing
+    sniff(prn=callback, iface=interface)
